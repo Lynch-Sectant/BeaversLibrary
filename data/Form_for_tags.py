5e41109ea9393ddd5b5df88c35aac864b16f770a
Артём  Азука <75079994+ArtyomAzuka@users.noreply.github.com>
2021-03-29 16:05:16 +0300

    Update Form_for_tags.py

--- data/Form_for_tags.py
+++ data/Form_for_tags.py
@@ -1,1 +1,10 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
 
+
+class Tag(SqlAlchemyBase):
+    __tablename__ = 'tags'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
