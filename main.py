fbc08fabd4465ce6911981ebb6932ec25f3d6698
Артём  Азука <75079994+ArtyomAzuka@users.noreply.github.com>
2021-04-25 20:57:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def my_page():
 
 
 def main():
-    db_session.global_init('bd.bd')
+    db_session.global_init('db.db')
     port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', port=port)
 
