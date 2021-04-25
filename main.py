f22449f3b90fc7af4c4ecb44e7eff62a2580cf71
Артём  Азука <75079994+ArtyomAzuka@users.noreply.github.com>
2021-04-25 23:16:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @login_manager.user_loader
 def load_user(user_id):
