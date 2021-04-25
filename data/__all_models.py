dcd57b0be7a6cc2af0417c6ee1fc956c58c91993
Артём  Азука <75079994+ArtyomAzuka@users.noreply.github.com>
2021-04-25 23:48:21 +0300

    Update __all_models.py

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,3 +1,3 @@
-from . import Form_for_users
-from . import Form_for_books
-from . import Form_for_tags
+from .Form_for_users import User
+from .Form_for_books import Book
+from .Form_for_tags import Tag
