5d4ca534d9977e14d6a9b2cf42a443ea5f9c44c5
Lesnichiy26 <75080073+Lesnichiy26@users.noreply.github.com>
2021-04-26 16:52:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, redirect
 import os
+from data import db_session
+from flask_wtf import FlaskForm
 from forms.user import RegisterForm
 from forms.book import RedactorForm
-from data import db_session
 from flask_login import login_required
-from flask_wtf import FlaskForm
 import flask_login
 from flask_login import LoginManager
+
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
-@app.route('/') # тест проги
+
+@app.route('/')  # тест проги
 def start():
     greeting = f'''<html>
         <head>
@@ -36,7 +39,7 @@ def new_book():
     form = RedactorForm()
     if form.validate_on_submit():
         if len(form.text.data) <= 40000:
-        length = 'короткий'
+            length = 'короткий'
     elif len(form.text.data) < 40000 and len(form.text.data) <= 80000:
         length = 'средний'
     elif len(form.text.data) > 80000:
@@ -46,7 +49,7 @@ def new_book():
             author=form.author.data,
             sinopsis=form.sinopsis.data,
             text=form.text.data,
-            tag_from_length = length,
+            tag_from_length=length,
             tag=form.tag.data
         )
         db_session.add(book)
@@ -85,7 +88,8 @@ def reqister():
 def my_page():
     db_sess = db_session.create_session()
     if db_sess.query(db_session.Book).filter(db_session.Book.author == User.current_user).all():
-        my_books = [i[1] for i in db_sess.query(db_session.Book).filter(db_session.Book.author == User.current_user).all()]
+        my_books = [i[1] for i in
+                    db_sess.query(db_session.Book).filter(db_session.Book.author == User.current_user).all()]
         return render_template('profile.html', title='Моя страница', user_books=my_books)
     else:
         my_books = ['У вас нет книг']
@@ -100,3 +104,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
